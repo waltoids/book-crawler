28b4080cedc752f15a435c8886510074ddb7e307
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-30 21:17:26 -0400

    boilerplate starter code

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
