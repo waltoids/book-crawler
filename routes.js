a02d7419743a757867d3c31ce308caafbf495e1e
Carolene Wilson <carolenesw@gmail.com>
2020-08-01 13:24:40 -0400

    search
    use handlebars #each method loop to get books in card

--- routes.js
+++ routes.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const routes = express.Router();
 
-
+// create route for index file 
 routes.get('/', (req, res) => {
     res.render('index', {
       title: 'Login Page'
     });
   });
   
+  // create route for registration file
   routes.get('/register', (req, res) => {
     res.render('register', {
       title: 'Registration Page'
     });
   });
   
+  // create route for search file 
   routes.get('/search', (req, res) => {
     res.render('search', {
       title: 'Book Search Page'
