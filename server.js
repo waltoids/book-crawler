c8bc51b156528a158a24ebb6422d03869c5a19ba
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-03 17:24:32 -0400

    fixed routing issue with page not loading the routes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const exphbs = require("express-handlebars");
 const db = require("./models");
 
 // Import routes and give the server access to them.
-const routes = require('./routes/user');
+// const routes = require('./routes/user');
+require('./routes/user')(app);
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,7 +21,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(routes);
+// app.use(routes);
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
