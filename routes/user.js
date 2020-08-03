392867e680cb01cda48d0f97018f4423b8d17664
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-03 17:23:50 -0400

    fixed '/' route function, added '/add' function with dummy data for testing

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,31 @@ const db = require("../models");
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-     
-      db.User.findAll().then(function(dbUser) {
+
+    //  res.send('hello world')
+      db.Users.findAll().then(function(dbUser) {
         // res.json(dbUser);
         console.log(dbUser);
-        res.sendStatus(200);
+        res.json(dbUser);
       })
+      .catch(err => console.log(err))
+    });
+    // add user and password
+    app.get('/add', function(req, res) {
+      const data = {
+        name:'walter',
+        password: '2020'
+      }
+
+      let = { name, password } = data;
+
+      //insert into table
+      db.Users.create({
+        name,
+        password
+      })
+      
+      .then(users => res.redirect('/'))
       .catch(err => console.log(err))
     });
 }
