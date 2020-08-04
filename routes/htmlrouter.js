4787ad86b6856d429274ccee90534e3390e0893b
larry laconi <soverylarry@gmail.com>
2020-08-03 20:48:32 -0400

    changed routes to reflect HandlebarsJS

--- routes/htmlrouter.js
+++ routes/htmlrouter.js
@@ -5,20 +5,20 @@ module.exports = function(app) {
 
   // index route loads login.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../routes/login.html"));
+    res.sendFile(path.join(__dirname, "../routes/login"));
   });
 
   app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../routes/login.html"));
+    res.sendFile(path.join(__dirname, "../routes/login"));
   });
 
   
   app.get("/register", function(req, res) {
-    res.sendFile(path.join(__dirname, "../routes/register.html"));
+    res.sendFile(path.join(__dirname, "../routes/register"));
   });
 
   app.get("/search", function(req, res) {
-    res.sendFile(path.join(__dirname, "../routes/search.html"));
+    res.sendFile(path.join(__dirname, "../routes/search"));
   });
 
 };
