ca566b0f2d4f76fbc68928ff4bd708a1e34eb04e
Carolene Wilson <carolenesw@gmail.com>
2020-08-04 14:22:14 -0400

    app.js
    create on click function to capture book entered  data

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
     // Variables for parameters
-      var bookSearch = $('#book-search')
-      const submitBook = $(document).on("click", "#submitBook");
+    //   var bookSearch = $('#book-search')
+    //   const submitBook = $(document).on("click", "#submitBook");
       var bookList = $('.bookList')
-      
+    let bookSearch = "";
+
+    // create on click function to capture book data 
+      $("#submitBook").on("click", function (event) {
+        event.preventDefault();
+       bookSearch = $("#book-search").val().trim();
+       console.log(bookSearch)
+ 
+     
    // volume searches can be performed by sending an HTTP GET request to the following URL
+
+   // * search was not defined so I created a variable  for seach in a click function
+
     apiKey = "AIzaSyAf-0XFdRGP0OkXktMErBjkhJQKS8evcqE"
-    var queryUrl ="https://www.googleapis.com/books/v1/volumes?q=" + search + "&key=" + apiKey;
-  
+    var queryUrl ="https://www.googleapis.com/books/v1/volumes?q=" + bookSearch + "&key=" + apiKey;
+
+    console.log(bookSearch)
+
+    console.log(queryUrl)
+
     $(bookList).submit(function(){
       var search = $('#book-search').val();
            if(search == "")
@@ -41,4 +56,4 @@ $(document).ready(function(){
      return false;
     });
   });
-    
+})
