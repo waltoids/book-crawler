533d3ba6b8027963f9f906c11947371d55b45beb
Carolene Wilson <carolenesw@gmail.com>
2020-08-04 16:50:58 -0400

    Update app.js
    create loop to capture book data from array

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,59 +1,83 @@
-$(document).ready(function(){
-    // Variables for parameters
-    //   var bookSearch = $('#book-search')
-    //   const submitBook = $(document).on("click", "#submitBook");
-      var bookList = $('.bookList')
-    let bookSearch = "";
+// const { response } = require("express");
 
-    // create on click function to capture book data 
-      $("#submitBook").on("click", function (event) {
-        event.preventDefault();
-       bookSearch = $("#book-search").val().trim();
-       console.log(bookSearch)
- 
-     
-   // volume searches can be performed by sending an HTTP GET request to the following URL
+$(document).ready(function () {
+  // Variables for parameters
+  //   let bookSearch = $('#book-search')
+  //   const submitBook = $(document).on("click", "#submitBook");
+  let bookList = $(".bookList");
+  let bookSearch = "";
 
-   // * search was not defined so I created a variable  for seach in a click function
+  // create on click function to capture book entered data
+  $("#submitBook").on("click", function (event) {
+    event.preventDefault();
+    bookSearch = $("#book-search").val().trim();
+    console.log(bookSearch);
 
-    apiKey = "AIzaSyAf-0XFdRGP0OkXktMErBjkhJQKS8evcqE"
-    var queryUrl ="https://www.googleapis.com/books/v1/volumes?q=" + bookSearch + "&key=" + apiKey;
+    // volume searches can be performed by sending an HTTP GET request to the following URL
 
-    console.log(bookSearch)
+    // * search was not defined so I created a variable  for seach in a click function
 
-    console.log(queryUrl)
+    apiKey = "AIzaSyAf-0XFdRGP0OkXktMErBjkhJQKS8evcqE";
+    const queryUrl = "https://www.googleapis.com/books/v1/volumes?q=" + bookSearch +
+      "&key=" + apiKey;
 
-    $(bookList).submit(function(){
-      var search = $('#book-search').val();
-           if(search == "")
-           {
-               alert("Please enter something in the field");
-           }
-           else{		
-           var url = "";
-           var img = "";
-        var title = "";
-        var author = "";
-        
-        
-        $.ajax({
-          url: queryUrl,
-          method: "GET"
-        }).then(function(response){
-  
-            for(i=0;i<response.items.length;i++)
-            {
-             title=$( + response.items[i].volumeInfo.title);  
-             author=$( + response.items[i].volumeInfo.authors);
-             img = $(response.items[i].volumeInfo.infoLink); 	
-             url= response.items[i].volumeInfo.imageLinks.thumbnail;
-             img.attr('src', url);
-             
-            }
-           });
-        
+    console.log(bookSearch);
+
+    console.log(queryUrl);
+
+    // $(bookList).submit(function(){
+    //   var search = $('#book-search').val();
+    if (bookSearch == "") {
+      alert("Please enter something in the field");
+    } else {
+      let url = "";
+      let img = "";
+      let title = "";
+      let author = "";
+      let description = "";
+
+      $.ajax({
+        url: queryUrl,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+
+        // empty div before dumping new info
+        $("#bookList").empty();
+        //    loop through array to get book data for all listing/books
+        for (let x in response.items) {
+          console.log("x response:", response.items[x]);
+          title = response.items[x].volumeInfo.title;
+          console.log("book title:", title);
+          author = response.items[x].volumeInfo.authors[0];
+          console.log("author name:", author);
+          description = response.items[x].volumeInfo.description;
+          console.log("description:", description);
+          img = response.items[x].volumeInfo.imageLinks.thumbnail;
+          console.log("image:", img);
+          url = response.items[x].volumeInfo.infoLink;
+          console.log("url:", url);
         }
+
+        // * remove '$' and plus sign from variable values
+
+        //     for(i=0;i<response.items.length;i++)
+        //     {
+        //      title = (response.items[i].volumeInfo.title);
+        //      console.log("item listing:", response.items[i])
+        //      console.log("title:", title)
+        //      author = (response.items[i].volumeInfo.authors);
+        //      img = (response.items[i].volumeInfo.infoLink);
+        //      url = response.items[i].volumeInfo.imageLinks.thumbnail;
+        //      img.attr('src', url);
+        //      console.log("data return;", title, author, img, url)
+        //     }
+        //     console.log(author)
+      });
+    }
+
      return false;
-    });
   });
-})
+});
+
+// })
