6edeec6adf4e94261f57f483bbeb1001610b0f7f
Carolene Wilson <carolenesw@gmail.com>
2020-08-04 19:37:31 -0400

    app.js
    Push latest changes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,3 @@
-// const { response } = require("express");
-
 $(document).ready(function () {
   // Variables for parameters
   //   let bookSearch = $('#book-search')
@@ -18,8 +16,11 @@ $(document).ready(function () {
     // * search was not defined so I created a variable  for seach in a click function
 
     apiKey = "AIzaSyAf-0XFdRGP0OkXktMErBjkhJQKS8evcqE";
-    const queryUrl = "https://www.googleapis.com/books/v1/volumes?q=" + bookSearch +
-      "&key=" + apiKey;
+    const queryUrl =
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      bookSearch +
+      "&key=" +
+      apiKey;
 
     console.log(bookSearch);
 
@@ -30,35 +31,65 @@ $(document).ready(function () {
     if (bookSearch == "") {
       alert("Please enter something in the field");
     } else {
-      let url = "";
+      let book_url = "";
       let img = "";
-      let title = "";
-      let author = "";
-      let description = "";
+      let book_title = "";
+      let book_author = "";
+      let book_description = "";
+      let books = "";
 
       $.ajax({
         url: queryUrl,
         method: "GET",
       }).then(function (response) {
         console.log(response);
+        books = response.items;
+        console.log("books:", books);
 
         // empty div before dumping new info
-        $("#bookList").empty();
+        $(".bookList").empty();
         //    loop through array to get book data for all listing/books
-        for (let x in response.items) {
-          console.log("x response:", response.items[x]);
-          title = response.items[x].volumeInfo.title;
-          console.log("book title:", title);
-          author = response.items[x].volumeInfo.authors[0];
-          console.log("author name:", author);
-          description = response.items[x].volumeInfo.description;
-          console.log("description:", description);
-          img = response.items[x].volumeInfo.imageLinks.thumbnail;
+        for (let x in books) {
+          console.log("x response:", books[x]);
+          book_title = books[x].volumeInfo.title;
+          console.log("book title:", book_title);
+          book_author = books[x].volumeInfo.authors[0];
+          console.log("author name:", book_author);
+          book_description = books[x].volumeInfo.description;
+          console.log("description:", book_description);
+          img = books[x].volumeInfo.imageLinks.thumbnail;
           console.log("image:", img);
-          url = response.items[x].volumeInfo.infoLink;
-          console.log("url:", url);
+          book_url = books[x].volumeInfo.infoLink;
+          console.log("url:", book_url);
+
+          $(".bookList").append($(`
+          <div class="col mr2 books_return">
+                        <div class="row">
+                            <div class="input-field col col s4"></div>
+                            <div class="card blue-grey darken-1">
+                                <img class="bookImage" src="{{book_url}}" alt="">
+                                <div class="card-content white-text center">
+                                    <span class="card-title">{{book_title}}</span>
+                                    <p>{{book_author}}</p>
+                                    <p>{{book_description}}</p>
+                                    <img class="booksImg" src="{{book_img}}" alt="Three Books with leaves" width="200"
+                                        height="240">
+                                </div>
+                                <div class="card-action">
+                                    <a href="{{book_url}}">Preview the book</a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+          `));
         }
 
+
+
+
+        
         // * remove '$' and plus sign from variable values
 
         //     for(i=0;i<response.items.length;i++)
@@ -76,8 +107,27 @@ $(document).ready(function () {
       });
     }
 
-     return false;
+    return false;
   });
+
+// create book template 
+
+const booksTemplate = (books) => {
+const bookContainer = $("<div>").attr({
+    class: "books_return"
+});
+
+const img = $("<img>").attr({
+    src: img,
+    alt: ""
+});
+
+// const bookTitle = $("<span>").attr({
+//     class: 
+// });
+
+}
+
 });
 
 // })
