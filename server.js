679eed8057d1952b70deb3b8de897c02fd9154f5
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-04 20:32:23 -0400

    cleaned up server file, changed line 13 to false

--- server.js
+++ server.js
@@ -2,26 +2,22 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const db = require("./models");
 
-// Import routes and give the server access to them.
-// const routes = require('./routes/user');
-require('./routes/user')(app);
+const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Handlebars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// app.use(routes);
+require('./routes/user')(app);
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
