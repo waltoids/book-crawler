8695dde63888ce7acd0016842108a5dc229e9ed9
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-06 20:20:54 -0400

    linted file

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const db = require("./models");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const db = require('./models');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Handlebars middleware
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 require('./routes/user')(app);
 
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log('App listening on PORT ' + PORT);
   });
 });
