d98d112369f976fdca162660f50527512edeffc1
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-06 20:49:53 -0400

    linted file

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require('../models');
 
 module.exports = function(app) {
 
@@ -7,7 +7,7 @@ module.exports = function(app) {
     res.render('index', {
       title: 'Login Page',
       style: 'main'
-    })
+    });
   });
 
   //Register page
@@ -20,51 +20,49 @@ module.exports = function(app) {
 
   //Handle registering of users
   app.post('/register', function(req, res){
-    const { name, password, password2} = req.body
+    const { name, password, password2} = req.body;
     let errors = [];
-    
 
-  //Check required fields
+    //Check required fields
     if(!name || !password || !password2) {
       errors.push({ msg: 'Please fill in all fields' });
-      
     }
 
     if(password !== password2) {
       errors.push({ msg: 'Passwords do not match' });
     }
 
-  //Check password length
+    //Check password length
     if(password.length < 6){
       errors.push({ msg: 'Password should be at least 6 characters' });
     }
-    
+
     if(errors.length > 0){
       res.render('register',{
         title: 'Registration Page',
         style: 'main',
         errors,
         name
-      })
-      console.log(errors)
+      });
+      console.log(errors);
     }else {
       //Validation passed
-      db.Users.findOne({ 
+      db.Users.findOne({
         where: {
           name:name
         }
-       })
+      })
         .then(function(user){
           if(user) {
             //User exists
-            errors.push({ msg: 'User is already registered' })
+            errors.push({ msg: 'User is already registered' });
             res.render('register',{
               title: 'Registration Page',
-            style: 'main',
+              style: 'main',
               errors,
               name
             });
-            console.log(errors)
+            console.log(errors);
           } else {
             db.Users.create({
               name,
@@ -78,31 +76,30 @@ module.exports = function(app) {
 
   //Login handler
   app.post('/', function(req, res){
-    const { name, password } = req.body
+    const { name, password } = req.body;
     let errors = [];
     //finds user based on name and password
-    db.Users.findOne({ 
+    db.Users.findOne({
       where: {
         name:name,
         password:password
       }
-     })
-     .then(function(user){
-       if(user){
-         //user and password matches
-         res.render('search', {
-          title: 'Book Search Page',
-          style: 'books'
-        })
-       }else{
-        errors.push({ msg: 'Incorrect Login!' });
-        res.render('index', {
-          title: 'Login Page',
-          style: 'main',
-          errors
-        })
-       }
-     })
-
-  })
-}
+    })
+      .then(function(user){
+        if(user){
+          //user and password matches
+          res.render('search', {
+            title: 'Book Search Page',
+            style: 'books'
+          });
+        }else{
+          errors.push({ msg: 'Incorrect Login!' });
+          res.render('index', {
+            title: 'Login Page',
+            style: 'main',
+            errors
+          });
+        }
+      });
+  });
+};
