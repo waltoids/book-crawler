3061e7112aa198dbbddc51f0194ef923f364f7d2
Walter Joy IV <waltoids@users.noreply.github.com>
2020-08-06 20:22:23 -0400

    linted file

--- models/users.js
+++ models/users.js
@@ -1,20 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
-    const Users = sequelize.define("Users", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                len: [3,25]
-              }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        }
-    });
-    return Users
+  const Users = sequelize.define('Users', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3,25]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Users;
 };
